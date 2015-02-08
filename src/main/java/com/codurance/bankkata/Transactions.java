284909cd1abd74773edaf330761f9ea58a6c3418
Sandro Mancuso <sandromancuso@yahoo.com>
2015-02-08 23:31:14 +0200

    Transactions implemented

--- src/main/java/com/codurance/bankkata/Transactions.java
+++ src/main/java/com/codurance/bankkata/Transactions.java
@@ -1,15 +1,21 @@
 package com.codurance.bankkata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Transactions {
 
+	private List<Transaction> transactionList;
+
+	public Transactions() {
+		this.transactionList = new ArrayList<>();
+	}
+
 	public void add(Transaction transaction) {
-
-
+		transactionList.add(0, transaction);
 	}
 
 	public List<Transaction> all() {
-		return null;
+		return transactionList;
 	}
 }
