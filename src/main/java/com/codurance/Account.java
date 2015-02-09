bf92a4038022f6dd9f70ac1904e306e69c2d94bd
Sandro Mancuso <sandromancuso@yahoo.com>
2015-02-09 23:55:33 +0200

    PrintStatementFeature test

---
+++ src/main/java/com/codurance/Account.java
@@ -0,0 +1,20 @@
+package com.codurance;
+
+public class Account {
+
+	public void deposit(int amount) {
+
+
+	}
+
+	public void withdraw(int amount) {
+
+
+	}
+
+	public void printStatement() {
+
+
+	}
+
+}
---
+++ src/test/java/acceptance/PrintStatementFeature.java
@@ -0,0 +1,37 @@
+package acceptance;
+
+import com.codurance.Account;
+import com.codurance.bankkata.Console;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PrintStatementFeature {
+
+	@Mock Console console;
+	private Account account;
+
+	@Before
+	public void initialise() {
+	    account = new Account();
+	}
+
+	@Test public void
+	should_print_statement_containing_all_transactions() {
+		account.deposit(1000);
+		account.withdraw(100);
+		account.deposit(500);
+
+		account.printStatement();
+
+		verify(console).printLine("DATE | AMOUNT | BALANCE");
+		verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
+		verify(console).printLine("02/04/2014 | -100.00 | 900.00");
+		verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
+	}
+}
